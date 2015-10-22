9d4b68fb80ed82e4fdd8b94351d641a825bce1e3
DieAD <1194672376@qq.com>
2015-10-22 09:12:13 +0800

    Project
    10-22

---
+++ Allin/dlivery_client/src/main/java/nju/ztww/vo/OrderVO.java
@@ -0,0 +1,18 @@
+package nju.ztww.vo;
+
+import nju.ztww.common.Customer;
+import nju.ztww.common.OrderInfo;
+
+public class OrderVO {
+	public final String idOfOrder;
+	public final Customer customer;
+	public final OrderInfo info;
+	
+	public OrderVO(String idOfOrder,Customer customer,OrderInfo info){
+		this.idOfOrder = idOfOrder;
+		this.customer=  customer;
+		this.info = info;
+	}
+	
+
+}
