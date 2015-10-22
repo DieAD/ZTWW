0edd278e3d84757d20a864679dc4b13be51067ca
Catherinena <574715653@qq.com>
2015-10-22 11:31:38 +0800

    10/22/11:30

--- Allin/dlivery_client/src/main/java/nju/ztww/service/UserService.java
+++ Allin/dlivery_client/src/main/java/nju/ztww/service/UserService.java
@@ -1,5 +1,24 @@
 package nju.ztww.service;
 
 public interface UserService {
-
+/*
+ * 用户输入账号密码验证身份
+ * 输入：String id : 用户的ID
+ * 输入：String passward : 对应的密码
+ * 输出：MemberVO : 该账号对应的用户信息，若不匹配则返回Null
+ */
+	public MemberVO login(String id, String password);
+	
+/*
+ * 用户输出正确的订单号格式后调用该函数调出订单信息
+ * 输入：String order : 订单号
+ * 输出：OrderVO : 订单信息VO
+ */
+	public OrderVO checkOrder(String order);
+	
+/*
+ * 用户处理某践行需要权限的事件时调用该函数，返回目前操作的用户的身份
+ * 输出：MemberVO : 该页面操作的用户身份	
+ */
+	public position getState();
 }
---
+++ Allin/dlivery_client/src/main/java/nju/ztww/vo/SalaryVO.java
@@ -0,0 +1,10 @@
+package nju.ztww.vo;
+
+public class StorageListVO {
+    public final String id;
+    public final String name;
+    public final pattern type;
+    public final double salary;
+    
+   
+}
