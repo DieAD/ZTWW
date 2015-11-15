68f27aefc59dcd6524240e242653b189652b071c
Catherinena <574715653@qq.com>
2015-11-15 17:49:13 +0800

    123

--- Allin/dlivery_client/src/test/java/FinanceTester/TestGetFund.java
+++ Allin/dlivery_client/src/test/java/FinanceTester/TestGetFund.java
@@ -1,14 +1,19 @@
 package FinanceTester;
 
+import static org.junit.Assert.*;
+import nju.ztww.Mock_Finance.MockFinance;
+import nju.ztww.Mock_Finance.MockFund;
 import nju.ztww.bl.finance.FinanceBL;
+import nju.ztww.vo.AccountVO;
 
 import org.junit.Test;
 
-public class TestAddDiag {
+public class TestGetFund {
+	@SuppressWarnings("deprecation")
 	@Test
-	public void testAddDiag(){
+	public void testGetFund(){
 		FinanceBL finance = new FinanceBL();
-		finance.addDiag();
-		equals(true);
+		MockFund fund = new MockFund(0);
+		assertEquals(0.1, fund.getFund());
 	}
 }
