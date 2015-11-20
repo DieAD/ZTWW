e33648e0ea115b1ff9022d1503cf630be5fa2152
Catherinena <574715653@qq.com>
2015-11-21 01:00:29 +0800

    11/21

--- Allin/dlivery_client/src/main/java/nju/ztww/ui/manage/GManager_CheckOrders.java
+++ Allin/dlivery_client/src/main/java/nju/ztww/ui/manage/GManager_CheckOrders.java
@@ -1,9 +1,14 @@
 package nju.ztww.ui.manage;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
 
 public class GManager_CheckOrders extends JPanel{
 //		JTextField search = new JTextField();
@@ -28,7 +33,19 @@ public class GManager_CheckOrders extends JPanel{
 		JButton order8Button = new JButton();
 		JButton order9Button = new JButton();
 		JButton order10Button = new JButton();
-		
+		JButton sureButton = new JButton();
+		MyTable table;
+		JPanel tablePanel = new JPanel();		
+//		Object[][] orders1;
+//		Object[][] orders2;
+//		Object[][] orders3;
+//		Object[][] orders4;
+//		Object[][] orders5;
+//		Object[][] orders6;
+//		Object[][] orders7;
+//		Object[][] orders8;
+//		Object[][] orders9;
+//		Object[][] orders10;
 		public GManager_CheckOrders(){
 //			this.setLayout(null);
 //			search.setBounds(20, 20, 100, 20);
@@ -39,10 +56,204 @@ public class GManager_CheckOrders extends JPanel{
 //			numLabel.setText("机构编号");
 ////			numLabel.setBounds(50, 100, width, height);
 			this.setLayout(null);
-			this.setVisible(true);
-			
+			tablePanel.setLayout(null);
+			tablePanel.setBounds(135,10,570,400);
+			this.add(tablePanel);
 			order1Button.setText("寄件单");
-			order1Button.setBounds(10, 10, 50, 20);
+			order1Button.setBounds(10, 10, 120, 30);
+			order2Button.setText("装车单");
+			order2Button.setBounds(10, 40, 120, 30);
+			order3Button.setText("营业厅到达单");
+			order3Button.setBounds(10, 70, 120, 30);
+			order4Button.setText("收款单");
+			order4Button.setBounds(10, 100, 120, 30);			
+			order5Button.setText("派件单");
+			order5Button.setBounds(10, 130, 120, 30);
+			order6Button.setText("中装中心到达单");
+			order6Button.setBounds(10, 160, 120, 30);
+			order7Button.setText("入库单");
+			order7Button.setBounds(10, 190, 120, 30);
+			order8Button.setText("中转单");
+			order8Button.setBounds(10, 220, 120, 30);
+			order9Button.setText("出库单");
+			order9Button.setBounds(10, 250, 120, 30);			
+			order10Button.setText("付款单");
+			order10Button.setBounds(10, 280, 120, 30);
+			sureButton.setText("审批单据");
+			sureButton.setBounds(550, 420, 120, 30);
+			order1Button.addActionListener(new ActionListener() {				
+
+				public void actionPerformed(ActionEvent e) {
+					// TODO Auto-generated method stub
+					Object[][] Orders1 = {
+							{new Boolean(true), "123456789", "提交状态", "Tom", "寄件单"},
+							{new Boolean(true), "123456789", "提交状态", "Tom", "寄件单"}
+							
+					};
+				
+					 table = new MyTable(Orders1);
+					 tablePanel.add(table);
+					
+				}
+			});
+			order2Button.addActionListener(new ActionListener() {
+
+				
+				public void actionPerformed(ActionEvent e) {
+					// TODO Auto-generated method stub
+					Object[][] Orders2 = {
+							{false, "12345672345", "提交状态", "Jerry", "装车单"}
+					};
+					table = null;
+					 table = new MyTable(Orders2);
+					 tablePanel.removeAll();
+					 tablePanel.add(table);
+					 tablePanel.repaint();
+				}
+			});
+			order3Button.addActionListener(new ActionListener() {
+
+				
+				public void actionPerformed(ActionEvent e) {
+					// TODO Auto-generated method stub
+					Object[][] Orders3 = {
+							{false, "0501647", "提交状态", "Jerry3", "营业厅到达单"}
+					};
+				
+					 table = new MyTable(Orders3);
+					 tablePanel.removeAll();
+					 tablePanel.add(table);
+					 tablePanel.repaint();
+				}
+			});
+			order4Button.addActionListener(new ActionListener() {
+
+				
+				public void actionPerformed(ActionEvent e) {
+					// TODO Auto-generated method stub
+					Object[][] Orders4 = {
+							{false, "12345672345", "提交状态", "Jerry", "收款单"}
+					};
+				
+					 table = new MyTable(Orders4);
+					 tablePanel.removeAll();
+					 tablePanel.add(table);
+					 tablePanel.repaint();
+				}
+			});
+			order5Button.addActionListener(new ActionListener() {
+
+				
+				public void actionPerformed(ActionEvent e) {
+					// TODO Auto-generated method stub
+					Object[][] Orders5 = {
+							{false, "12345672345", "提交状态", "Jerry", "派件单"}
+					};
+				
+					 table = new MyTable(Orders5);
+					 tablePanel.removeAll();
+					 tablePanel.add(table);
+					 tablePanel.repaint();
+				}
+			});
+			order6Button.addActionListener(new ActionListener() {
+
+				
+				public void actionPerformed(ActionEvent e) {
+					// TODO Auto-generated method stub
+					Object[][] Orders6 = {
+							{false, "12345672345", "提交状态", "Jerry", "中转中心到达单"}
+					};
+				
+					 table = new MyTable(Orders6);
+					 tablePanel.removeAll();
+					 tablePanel.add(table);
+					 tablePanel.repaint();
+				}
+			});
+			order7Button.addActionListener(new ActionListener() {
+
+				
+				public void actionPerformed(ActionEvent e) {
+					// TODO Auto-generated method stub
+					Object[][] Orders7 = {
+							{false, "12345672345", "提交状态", "Jerry", "入库单"}
+					};
+				
+					 table = new MyTable(Orders7);
+					 tablePanel.removeAll();
+					 tablePanel.add(table);
+					 tablePanel.repaint();
+				}
+			});			
+			order8Button.addActionListener(new ActionListener() {
+
+				
+				public void actionPerformed(ActionEvent e) {
+					// TODO Auto-generated method stub
+					Object[][] Orders8 = {
+							{false, "12345672345", "提交状态", "Jerry", "中转单"}
+					};
+				
+					 table = new MyTable(Orders8);
+					 tablePanel.removeAll();
+					 tablePanel.add(table);
+					 tablePanel.repaint();
+				}
+			});			
+			order9Button.addActionListener(new ActionListener() {
+
+				
+				public void actionPerformed(ActionEvent e) {
+					// TODO Auto-generated method stub
+					Object[][] Orders9 = {
+							{false, "12345672345", "提交状态", "Jerry", "出库单"}
+					};
+				
+					 table = new MyTable(Orders9);
+					 tablePanel.removeAll();
+					 tablePanel.add(table);
+					 tablePanel.repaint();
+				}
+			});
+			order10Button.addActionListener(new ActionListener() {
+				
+				public void actionPerformed(ActionEvent e) {
+					// TODO Auto-generated method stub
+					Object[][] Orders10 = {
+							{false, "12345672345", "提交状态", "Jerry", "付款单"}
+					};
+				
+					 table = new MyTable(Orders10);
+					 tablePanel.removeAll();
+					 tablePanel.add(table);
+					 tablePanel.repaint();
+				}
+			});			
+			
 			this.add(order1Button);
+			this.add(order2Button);
+			this.add(order3Button);
+			this.add(order4Button);
+			this.add(order5Button);
+			this.add(order6Button);
+			this.add(order7Button);
+			this.add(order8Button);
+			this.add(order9Button);
+			this.add(order10Button);
+			this.add(sureButton);
 		}
+		
+//	ActionListener e1 = new ActionListener() {
+//		
+//		public void actionPerformed(ActionEvent e) {
+//			// TODO Auto-generated method stub
+//			Object[][] Orders = {
+//					{" ", "123456789", "提交状态", "Tom", "......"}
+//			};
+//			MyTable table1 = new MyTable(Orders);
+//			table1.setBounds(210,60,690,400);
+//			
+//		}
+//	};	
 }
--- Allin/dlivery_client/src/main/java/nju/ztww/ui/manage/GManager_ManageOrgaUI.java
+++ Allin/dlivery_client/src/main/java/nju/ztww/ui/manage/GManager_ManageOrgaUI.java
@@ -1,5 +1,121 @@
 package nju.ztww.ui.manage;
 
-public class GManager_ManageOrgaUI {
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class GManager_ManageOrgaUI extends JPanel{
+	String[] position = {"快递员", "营业厅业务员", "中转中心业务员", "中转中心仓库管理人员", "财务人员", "总经理", "管理员"};
+	JButton searchButton = new JButton();
+	JButton modifyButton = new JButton();
+	JButton deletebButton = new JButton();
+	JLabel nameLabel = new JLabel();
+	JLabel idLabel = new JLabel();
+	JLabel membersLabel = new JLabel();
+	JTextField search = new JTextField();
+	JTextField name = new JTextField();
+	JTextField id = new JTextField();
+	JTextField members1 = new JTextField();
+	JTextField members2 = new JTextField();
+	JTextField members3 = new JTextField();
+	JDialog delete;
+	JButton yesButton = new JButton();
+	JButton cancelButton = new JButton();
+	JLabel tips = new JLabel();
+	
+	java.awt.Dimension screenSize = java.awt.Toolkit.getDefaultToolkit()
+			.getScreenSize();
+	
+	JComboBox mem1 = new JComboBox(position);
+	JComboBox mem2 = new JComboBox(position);
+	JComboBox mem3 = new JComboBox(position);
+	
+	public GManager_ManageOrgaUI(){
+		this.setLayout(null);
+		search.setBounds(60, 20, 160, 30);
+		searchButton.setText("查询");
+		searchButton.setBounds(240, 20, 80, 30);
+		
+		nameLabel.setText("机构名称");
+		nameLabel.setBounds(140, 100, 100, 30);
+		name.setBounds(240, 100, 180, 30);
+		
+		idLabel.setText("机构编号");
+		idLabel.setBounds(140, 150, 100, 30);
+		id.setBounds(240, 150, 180, 30);
+		
+		membersLabel.setText("机构内部成员");
+		membersLabel.setBounds(140, 200, 100, 30);
+		mem1.setBounds(240, 200, 160, 30);
+		members1.setBounds(440, 200, 100, 30);
+		mem2.setBounds(240, 250, 160, 30);
+		members2.setBounds(440, 250, 100, 30);
+		mem3.setBounds(240, 300, 160, 30);
+		members3.setBounds(440, 300, 100, 30);
+		
+		modifyButton.setText("修改");
+		modifyButton.setBounds(540,400,80,30);
+		
+		deletebButton.setText("删除");
+		deletebButton.setBounds(430, 400, 80, 30);
+		deletebButton.addActionListener(new ActionListener() {
+			
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				tips.setText("确定删除该机构吗？");
+				tips.setBounds(50, 20, 150, 20);
+				
+				yesButton.setText("确定");
+				yesButton.setBounds(130, 70, 60, 20);
+				yesButton.addActionListener(new ActionListener() {
+					
+					public void actionPerformed(ActionEvent e) {
+						// TODO Auto-generated method stub
+						delete.setVisible(false);
+					}
+				});
+				cancelButton.setText("取消");
+				cancelButton.setBounds(200, 70, 60, 20);
+				cancelButton.addActionListener(new ActionListener() {
+					
+					public void actionPerformed(ActionEvent e) {
+						// TODO Auto-generated method stub
+						delete.setVisible(false);
+					}
+				});
+				delete = new JDialog();
+				delete.setLayout(null);
+				delete.setVisible(true);
+				delete.setSize(new Dimension(300, 150));
+				delete.setLocation((screenSize.width)/2, (screenSize.height)/2);
+				delete.add(tips);
+				delete.add(yesButton);
+				delete.add(cancelButton);
+				
+			}
+		});
+		
+		this.add(search);
+		this.add(searchButton);
+		this.add(nameLabel);
+		this.add(name);
+		this.add(idLabel); 
+		this.add(id);
+		this.add(membersLabel);
+		this.add(mem1);
+		this.add(members1);
+		this.add(mem2);
+		this.add(members2);
+		this.add(mem3);
+		this.add(members3);
+		this.add(modifyButton);
+		this.add(deletebButton);
+	}
 }
