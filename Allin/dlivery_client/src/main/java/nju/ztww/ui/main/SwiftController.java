a0a6e73707231178577209b3a5c58ed3c9798d80
DieAD <1194672376@qq.com>
2015-11-21 20:36:11 +0800

    WH

--- Allin/dlivery_client/src/main/java/nju/ztww/ui/main/SwiftController.java
+++ Allin/dlivery_client/src/main/java/nju/ztww/ui/main/SwiftController.java
@@ -8,6 +8,8 @@ import javax.swing.JPanel;
 import nju.ztww.ui.commodity.ClerkOfCenterUi;
 import nju.ztww.ui.commodity.StorageUi;
 import nju.ztww.ui.finance.FinanceUI;
+import nju.ztww.ui.manage.TestGManagerUI;
+import nju.ztww.ui.manage.TextManageUI;
 import nju.ztww.ui.order.TestCourierUI;
 import nju.ztww.ui.user.TextBussinessUI;
 
@@ -20,6 +22,8 @@ public class SwiftController {
 	private StorageUi storageUI;
 	private FinanceUI financeUI;
 	private ClerkOfCenterUi ccUI;
+	private TestGManagerUI GMUI;
+	private TextManageUI   MUI;
 	//
 	private JFrame frame;
 	private int id;
@@ -28,15 +32,17 @@ public class SwiftController {
 		courierUI = new TestCourierUI(frame);
 		businessUI = new TextBussinessUI(frame);
 		financeUI =  new FinanceUI(frame);
+		GMUI = new TestGManagerUI(frame);
 		storageUI = new StorageUi();
 		ccUI = new ClerkOfCenterUi();
+		MUI = new TextManageUI();
 	}
 	
 	
 	public void switchUI(){
 		if(true){
 			
-			panelList = ccUI.getPanelList();
+			panelList = MUI.getPanelList();
 		}
 	}
 	
@@ -45,7 +51,7 @@ public class SwiftController {
 		switchUI();
 	    frame.getContentPane().removeAll();
 	    for(JPanel panel : panelList){
-	    	panel.setVisible(true);
+	    //	panel.setVisible(true);
 	    	frame.add(panel);
 	    	frame.getContentPane().repaint();
 	    }
