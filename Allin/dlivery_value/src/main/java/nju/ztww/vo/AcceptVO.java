e7f89f34efa581bf91342e21a30ea8412eab6cbf
DieAD <1194672376@qq.com>
2015-11-25 19:45:18 +0800

    wh1125

--- Allin/dlivery_value/src/main/java/nju/ztww/vo/AcceptVO.java
+++ Allin/dlivery_value/src/main/java/nju/ztww/vo/AcceptVO.java
@@ -2,16 +2,20 @@ package nju.ztww.vo;
 
 public class AcceptVO extends OrderVO{
 	
+	public AcceptVO(int idOfOrder) {
+		super(idOfOrder);
+		// TODO Auto-generated constructor stub
+	}
 	private int idOfOrder;
 	private String businID;
 	private String acceptID;
 	private String accepter;
 	private String acceptTime;
-	private 
+	//private 
 	
 	
 	
-	public AcceptVO(int idOfOrder){
-		
-	}
+//	public AcceptVO(int idOfOrder){
+//		
+//	}
 }
