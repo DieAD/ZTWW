17d55b8eb301c6c0382d0e6bc6e405ed71aba362
DieAD <1194672376@qq.com>
2015-11-26 09:50:53 +0800

    wh

---
+++ Allin/dlivery_value/src/main/java/nju/ztww/vo/CollectionVO.java
@@ -0,0 +1,12 @@
+package nju.ztww.vo;
+
+public class CollectionVO {
+     public String date;
+     public double money;
+     public String courierid;
+     public String orderid;
+     
+     public CollectionVO(){
+    	 
+     }
+}
