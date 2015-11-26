9bc8b2f648561c3a6cf911d5c75762733b2293a6
Catherinena <574715653@qq.com>
2015-11-26 09:43:42 +0800

    123

--- Allin/dlivery_value/src/main/java/nju/ztww/vo/AcceptVO.java
+++ Allin/dlivery_value/src/main/java/nju/ztww/vo/AcceptVO.java
@@ -2,16 +2,31 @@ package nju.ztww.vo;
 
 public class AcceptVO extends OrderVO{
 	
-	private int idOfOrder;
-	private String businID;
-	private String acceptID;
-	private String accepter;
-	private String acceptTime;
-	private 
+	public int idOfOrder;
+	public String businID;
+	public String acceptID;
+	public String accepter;
+	public String acceptTime;
+	public String courierID;
+	public int state;
+	public int exe;
 	
 	
 	
 	public AcceptVO(int idOfOrder){
-		
+		super(idOfOrder);
+		this.idOfOrder = idOfOrder;
+	}
+	
+	public void setBusinID(String businID){
+		this.businID = businID;
+	}
+	
+	public String getBusinID(){
+		return businID;
+	}
+	
+	public void setAcceptID(String acceptID){
+		this.accepter
 	}
 }
