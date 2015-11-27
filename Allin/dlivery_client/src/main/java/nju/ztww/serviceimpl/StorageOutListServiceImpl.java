19b5ad584fd63dfba895ef3078f9c58a0a9e4594
wjwj123456 <wj14@software.nju.edu.cn>
2015-11-27 21:55:51 +0800

    WJ

--- Allin/dlivery_client/src/main/java/nju/ztww/serviceimpl/StorageOutListServiceImpl.java
+++ Allin/dlivery_client/src/main/java/nju/ztww/serviceimpl/StorageOutListServiceImpl.java
@@ -1,18 +1,19 @@
 package nju.ztww.serviceimpl;
-
+/**
+ * @author wangjie;
+ */
 import java.util.ArrayList;
 
-import nju.ztww.bl.commodity.StorageInofListBL;
-import nju.ztww.bl.commodity.StorageListOutbeforecheckBL;
+
 import nju.ztww.bl.commodity.StorageOutofListBL;
-import nju.ztww.po.StorageListLineofInPO;
+
 import nju.ztww.po.StorageListLineofOutPO;
 import nju.ztww.service.CommodityService;
 import nju.ztww.vo.OrderVO;
 
 import nju.ztww.vo.StorageListLineofInVO;
 import nju.ztww.vo.StorageListLineofOutVO;
-import nju.ztww.vo.StorageListVO;
+
 //主要用来实现增加出库信息的作用
 public class StorageOutListServiceImpl implements CommodityService {
 
@@ -53,7 +54,7 @@ public class StorageOutListServiceImpl implements CommodityService {
 
 	public void addoutOrder(ArrayList<StorageListLineofOutVO> arraylist,String idofcneter) {
 		// TODO Auto-generated method stub
-		System.out.println("在impl");
+		
 			ArrayList<StorageListLineofOutPO>arraylistinpo=new ArrayList<StorageListLineofOutPO>();
 			for(int i=0;i< arraylist.size();i++){
 				arraylistinpo.add(arraylist.get(i).changetopo());
