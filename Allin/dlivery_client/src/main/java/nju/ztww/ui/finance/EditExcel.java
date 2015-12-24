b0ac2a386a20d713cea9c6d1a01bd8fe5a306ae4
xiaomai <tianqihaoshuai@qq.com>
2015-12-24 22:43:05 +0800

    TQ

--- Allin/dlivery_client/src/main/java/nju/ztww/ui/finance/EditExcel.java
+++ Allin/dlivery_client/src/main/java/nju/ztww/ui/finance/EditExcel.java
@@ -1,8 +1,11 @@
 package nju.ztww.ui.finance;
 
+import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
@@ -13,15 +16,26 @@ import nju.ztww.vo.BenefitVO;
 
 public class EditExcel extends EditPanel {
 
-	JButton button1 = new JButton("经营情况表");
-	JButton button2 = new JButton("成本收益表");
+	JButton button1 = new JButton();
+	JButton button2 = new JButton();
 	public EditExcel(){
 		this.removeAll();
 		init();
 	}
+	public void paintComponent(Graphics g){
+		super.paintComponent(g);
+		
+		Image lable=new ImageIcon("photo/outBK.gif").getImage();
+		g.drawImage(lable, 0,0,null);
+		
+	}
 	public void init(){
-		button1.setBounds(10, 10, 80,30 );
-		button2.setBounds(10,70,80,30);
+		ImageIcon image1 = new ImageIcon("photo/ExportBenefitForm.png");
+		ImageIcon image2 = new ImageIcon("photo/ExportBusinessForm.png");
+		button1.setBounds(260, 55, 140,35 );
+		button1.setIcon(image2);
+		button2.setBounds(260,115,140,35);
+		button2.setIcon(image1);
 		button1.addActionListener(new Listener(this,1));
 		button2.addActionListener(new Listener(this,2));
 		this.add(button1);
